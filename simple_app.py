b064b827d9a11df3af432f8b8945d60c7a362067
bashhack <info@marclaughton.com>
2016-01-04 22:19:27 -0600

    passing html to route return

--- simple_app.py
+++ simple_app.py
@@ -12,6 +12,15 @@ def index(name='Test User'):
 @app.route('/add/<float:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
-    return '{} + {} = {}'.format(num1, num2, num1 + num2)
+    return '''
+    <!doctype html>
+    <html>
+    <head>
+        <title>Adding</title>
+    </head>
+    <body>
+        <h1>{} + {} = {}</h1>
+    </body>
+    '''.format(num1, num2, num1 + num2)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
