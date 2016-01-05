2afdb73721dc29eb26945b2a86d0a5ad99bc5c85
bashhack <info@marclaughton.com>
2016-01-04 22:05:32 -0600

    using converters in routes

--- simple_app.py
+++ simple_app.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/<name>')
 def index(name='Test User'):
-    name = request.args.get('name', name)
     return 'Hello, {}'.format(name)
+
+# use of converter to change type to int
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    # NOTE: expects a return value of type str
+    return '{} + {} = {}'.format(num1, num2, num1 + num2)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
