8317530ba61e2abc37a935340bccc67dda438f01
bashhack <info@marclaughton.com>
2016-01-04 21:37:58 -0600

    add request object

--- simple_app.py
+++ simple_app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Discourses of Dialectic: Precapitalist dialectic theory'
+def index(name='Test User'):
+    name = request.args.get('name', name)
+    return 'Hello, {}'.format(name)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
