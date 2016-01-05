01af17154b545ca8976ef39a08ac2015fc725bff
bashhack <info@marclaughton.com>
2016-01-04 23:30:11 -0600

    flat HTML

--- simple_app.py
+++ simple_app.py
@@ -6,13 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def index(name='Test User'):
-    return 'Hello, {}'.format(name)
+    context = { 'name': name }
+    return render_template('index.html', **context)
 
 @app.route('/add/<int:num1>/<int:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
-    return render_template('add.html')
+    context = { 'num1': num1, 'num2': num2 }
+    return render_template('add.html', **context)
 
 app.run(debug=True, port=8000, host='0.0.0.0')
