81ff64c34ab9f168d1a4ab733c7567ddae1eb468
bashhack <info@marclaughton.com>
2016-01-05 22:28:38 -0600

    working with cookies and JSON

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template, url_for, redirect, request
+import json
+
+from flask import (Flask, render_template, url_for, redirect,
+                   request, make_response)
+
+from options import DEFAULTS
 
 app = Flask(__name__)
 
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('character'))
+    except TypeError:
+        data = {}
+    return data
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = get_saved_data()
+    return render_template('index.html', saves=data)
+
+@app.route('/builder')
+def builder():
+    return render_template('builder.html',
+                           saves=get_saved_data(),
+                           options=DEFAULTS)
 
 @app.route('/save', methods=['POST'])
 def save():
-    import pdb; pdb.set_trace()
-    return redirect(url_for('index'))
+    response = make_response(redirect(url_for('builder')))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('character', json.dumps(data))
+    return response
 
 app.run(debug=True, host='0.0.0.0', port=8000)
