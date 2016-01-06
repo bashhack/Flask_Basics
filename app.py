7fb164b7a19180bfcff43370012a7bfd5c4338de
bashhack <info@marclaughton.com>
2016-01-05 21:11:15 -0600

    using the python debugger

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ def index():
 
 @app.route('/save', methods=['POST'])
 def save():
+    import pdb; pdb.set_trace()
     return redirect(url_for('index'))
 
 app.run(debug=True, host='0.0.0.0', port=8000)
