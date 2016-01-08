a583b1c13af246935d17dd29c0b6c4a7fd2649cd
bashhack <info@marclaughton.com>
2016-01-07 22:31:07 -0600

    refactor and cleanup
    Refactor per Python standards for clean and readable code

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+'''
+Main entrypoint for my 'build a bear' web application
+'''
+
 import json
 
 from flask import (Flask, render_template, url_for, redirect,
@@ -7,26 +11,40 @@ from options import DEFAULTS
 
 app = Flask(__name__)
 
+
+# This should work, but beware if this returns xyz...I get a 404, should
+# fix later...
 def get_saved_data():
+    ''' Get saved data from browser cookies,
+    or initialize data as empty dict '''
     try:
         data = json.loads(request.cookies.get('character'))
     except TypeError:
         data = {}
     return data
 
+
 @app.route('/')
 def index():
+    ''' Instantiates home/index root route,
+    context variable saves is assigned to dict data '''
     data = get_saved_data()
     return render_template('index.html', saves=data)
 
+
 @app.route('/builder')
 def builder():
+    ''' Set builder route, gets cookie data, options variable
+    is set from options.py '''
     return render_template('builder.html',
                            saves=get_saved_data(),
                            options=DEFAULTS)
 
+
 @app.route('/save', methods=['POST'])
 def save():
+    ''' Handles save method (to cookies), and redirect post-save to
+    builder route '''
     response = make_response(redirect(url_for('builder')))
     data = get_saved_data()
     data.update(dict(request.form.items()))
